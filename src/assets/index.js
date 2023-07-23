bf2bc6ab0b269075f985f9e6b7a2de99dfc07776
Muhammed Fahiz p <fahiz@Muhammeds-MacBook-Air.local>
2023-07-23 19:42:23 +0530

    add: assets folder for the 3d images'

---
+++ src/assets/index.js
@@ -0,0 +1,68 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+import next from "./tech/next.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import kafka from "./tech/kafka.png";
+import kubernetes from "./tech/kubernetes.png";
+
+import brototype from "./company/brototype.png";
+import ssmptc from "./company/ssmptc.jpeg";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import prompt from "./Prompt.png";
+import evara from "./Evara.png";
+import summarize from "./Summarize.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  kafka,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  tailwind,
+  typescript,
+  threejs,
+  next,
+  kubernetes,
+  carrent,
+  jobit,
+  tripguide,
+  prompt,
+  summarize,
+  evara,
+  ssmptc,
+  brototype,
+};
